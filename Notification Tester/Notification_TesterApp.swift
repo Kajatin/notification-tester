3b929f74e42518be58f2f9a51eff4404c415fab6
Roland Kajatin <roland.kajatin@gmail.com>
2023-05-09 09:04:17 +0200

    Initial Commit

---
+++ Notification Tester/Notification_TesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Notification_TesterApp.swift
+//  Notification Tester
+//
+//  Created by Roland Kajatin on 09/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Notification_TesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
